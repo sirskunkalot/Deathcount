ebd77fdb24ef2d208dc0a8b7c4df5d6894e45e2b
Jules <github@sirskunkalot.de>
2026-02-11 23:55:02 +0100

    feat: statistics UI

--- Deathcount/Deathcount.cs
+++ Deathcount/Deathcount.cs
@@ -1,11 +1,10 @@
 using BepInEx;
 using BepInEx.Configuration;
 using Jotunn.Configs;
-using Jotunn.Entities;
 using Jotunn.Managers;
+using System;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Profiling;
-using static UnityEngine.UI.GridLayoutGroup;
 
 namespace Deathcount
 {
@@ -23,18 +22,24 @@ namespace Deathcount
 
         private static GameObject StatisticsUI;
         
-        // Use this class to add your own localization to the game
-        // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
-        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
-
         private void Awake()
         {
             EnabledConfig = Config.Bind(
                 "Settings", "Enabled", true, "Deathcount is enabled and will show total deaths on screen upon entering a world");
+            EnabledConfig.SettingChanged += (_, _) =>
+            {
+                if (EnabledConfig.Value)
+                {
+
+                }
+                else
+                {
+                    DestroyStatisticsUI();
+                }
+            };
 
             ToggleUIConfig = Config.Bind(
-                "Settings", "Detailed Death Statistics UI", KeyCode.F10, "Key to show/hide the death statistics UI");
-            
+                "Settings", "Death Statistics UI", KeyCode.F10, "Key to show/hide the death statistics UI");
             ToggleUIButton = new ButtonConfig
             {
                 Name = "DeathcountToggle",
@@ -50,91 +55,106 @@ namespace Deathcount
             if (!EnabledConfig.Value) {
                 return;
             }
-            
-            // No keys without ZInput
             if (ZInput.instance == null)
             {
                 return;
             }
 
-            // Toggle UI
-            if (EnabledConfig.Value && ZInput.GetButtonDown(ToggleUIButton.Name))
+            if (ZInput.GetButtonDown(ToggleUIButton.Name))
             {
                 if (!StatisticsUI)
                 {
-                    CreateUI();
+                    CreateStatisticsUI();
                 }
                 else
                 {
-                    DestroyUI();
+                    DestroyStatisticsUI();
                 }
             }
         }
 
-        private static void CreateUI()
+        private static void CreateStatisticsUI()
         {
             if (GUIManager.Instance == null)
             {
                 Jotunn.Logger.LogError("GUIManager instance is null");
                 return;
             }
-
             if (!GUIManager.CustomGUIFront)
             {
                 Jotunn.Logger.LogError("GUIManager CustomGUI is null");
                 return;
             }
-
             if (StatisticsUI)
             {
                 Jotunn.Logger.LogError("StatisticsUI is not null");
                 return;
             }
-
-            // Create the panel object
-            StatisticsUI = GUIManager.Instance.CreateWoodpanel(
-                parent: GUIManager.CustomGUIFront.transform,
-                anchorMin: new Vector2(0.5f, 0.5f),
-                anchorMax: new Vector2(0.5f, 0.5f),
-                position: new Vector2(0, 0),
-                width: 850,
-                height: 600,
-                draggable: false);
-            StatisticsUI.SetActive(false);
-
-            // Create the text objects
-            GUIManager.Instance.CreateText(
-                text: "Death Statistics",
-                parent: StatisticsUI.transform,
-                anchorMin: new Vector2(0.5f, 1f),
-                anchorMax: new Vector2(0.5f, 1f),
-                position: new Vector2(0f, -50f),
-                font: GUIManager.Instance.AveriaSerifBold,
-                fontSize: 30,
-                color: GUIManager.Instance.ValheimOrange,
-                outline: true,
-                outlineColor: Color.black,
-                width: 350f,
-                height: 40f,
-                addContentSizeFitter: false);
             
-            var profile = Game.instance.GetPlayerProfile();
-            if (profile != null)
-            {
-                GUIManager.Instance.CreateText($"Overall deaths: {profile.m_playerStats[PlayerStatType.Deaths]}",
-                    StatisticsUI.transform, new Vector2(0.5f, 1f), new Vector2(0.5f, 1f), new Vector2(0f, 0f),
-                    GUIManager.Instance.AveriaSerifBold, 30, GUIManager.Instance.ValheimOrange,
-                    true, Color.black, 650f, 40f, false);
+            if (Game.instance) {
+                StatisticsUI = GUIManager.Instance.CreateWoodpanel(
+                    parent: GUIManager.CustomGUIFront.transform,
+                    anchorMin: new Vector2(0.5f, 0.5f),
+                    anchorMax: new Vector2(0.5f, 0.5f),
+                    position: new Vector2(0, 0),
+                    width: 450f,
+                    height: 500f,
+                    draggable: false);
+                //StatisticsUI.AddComponent<DragWindowCntrl>();
+                StatisticsUI.SetActive(false);
+
+                GUIManager.Instance.CreateText(
+                    text: "Death Statistics",
+                    parent: StatisticsUI.transform,
+                    anchorMin: new Vector2(0.5f, 1f),
+                    anchorMax: new Vector2(0.5f, 1f),
+                    position: new Vector2(45f, -45f),
+                    font: GUIManager.Instance.AveriaSerifBold,
+                    fontSize: 30,
+                    color: GUIManager.Instance.ValheimOrange,
+                    outline: true,
+                    outlineColor: Color.black,
+                    width: 300f,
+                    height: 40f,
+                    addContentSizeFitter: false);
+            
+                var scrollView = GUIManager.Instance.CreateScrollView(
+                    StatisticsUI.transform,
+                    false, true, 10f, 5f,
+                    GUIManager.Instance.ValheimScrollbarHandleColorBlock, Color.black,
+                    300f, 350f);
+
+                var viewport =
+                    scrollView.transform.Find("Scroll View/Viewport/Content") as RectTransform;
+
+                var profile = Game.instance.GetPlayerProfile();
+                if (profile != null)
+                {
+                    GUIManager.Instance.CreateText($"Deaths: {profile.m_playerStats[PlayerStatType.Deaths]}",
+                        viewport.transform, new Vector2(0.5f, 1f), new Vector2(0.5f, 1f), new Vector2(0f, 0f),
+                        GUIManager.Instance.AveriaSerif, 20, GUIManager.Instance.ValheimOrange,
+                        true, Color.black, 300f, 40f, false);
+
+                    var deathValues = Enum.GetValues(typeof(PlayerStatType))
+                        .Cast<PlayerStatType>()
+                        .Where(e => e.ToString().StartsWith("DeathB") || e.ToString().StartsWith("Tombstone"))
+                        .OrderBy(e => e.ToString());
+
+                    foreach (var value in deathValues)
+                    {
+                        GUIManager.Instance.CreateText($"{value}: {profile.m_playerStats[value]}",
+                            viewport.transform, new Vector2(0.5f, 1f), new Vector2(0.5f, 1f), new Vector2(0f, 0f),
+                            GUIManager.Instance.AveriaSerif, 20, GUIManager.Instance.ValheimOrange,
+                            true, Color.black, 300f, 40f, false);
+                    }
+                }
+                
+                StatisticsUI.SetActive(true);
+                GUIManager.BlockInput(true);
             }
-            
-            // Set the active state of the panel
-            StatisticsUI.SetActive(true);
-
-            // Toggle input for the player and camera while displaying the GUI
-            GUIManager.BlockInput(true);
         }
 
-        private static void DestroyUI()
+        private static void DestroyStatisticsUI()
         {
             if (!StatisticsUI)
             {
